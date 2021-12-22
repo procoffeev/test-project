eec9a55f96d66c1c1cfa9978b27e75a3e41d912d
Procoffeev <tukanlabs@gmail.com>
2021-12-22 18:08:33 +0500

    selector is placed in an external variable

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,8 +3,8 @@ from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(By.CSS_SELECTOR, LOGIN_LINK)
         login_link.click()
         
     def should_be_login_link(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
+        self.browser.find_element(By.CSS_SELECTOR, LOGIN_LINK), "Login link is not presented"
