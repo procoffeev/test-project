2c08fc19924bab5f10f7adf772fe01073c2a71de
Procoffeev <tukanlabs@gmail.com>
2021-12-22 18:15:42 +0500

    fixed link promotion

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,10 +1,11 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, LOGIN_LINK)
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
         
     def should_be_login_link(self):
-        self.browser.find_element(By.CSS_SELECTOR, LOGIN_LINK), "Login link is not presented"
+        self.browser.find_element(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
